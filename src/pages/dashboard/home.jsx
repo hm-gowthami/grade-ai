a4aa1cb59885a273b16b9e442f27dafe98765cd7
hm-gowthami <gowthami.uppula@happiestminds.com>
2024-04-10 01:38:17 +0530

    chore:dashborad dummy

--- src/pages/dashboard/home.jsx
+++ src/pages/dashboard/home.jsx
@@ -1,11 +1,38 @@
 import React from "react";
-
+import { Card, Typography } from "@material-tailwind/react";
 export function Home() {
   return (
-    <h3 class="block font-sans text-9xl antialiased font-semibold leading-tight tracking-normal text-inherit">
-    Get Ready for the test
-  </h3>
-
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        margin: "30px",
+      }}
+    >
+      <h3 class="block font-sans text-9xl antialiased font-semibold leading-tight tracking-normal text-inherit">
+        Get Ready for the test
+      </h3>
+      <Card
+        style={{
+          width: "30%",
+          height: "300px",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          margin: "50px",
+        }}
+      >
+        <Typography style={{ fontSize: "30px", color: "blue" }}>
+          User Name:Gowthami
+        </Typography>
+        <Typography style={{ fontSize: "30px", color: "blue" }}>
+          Role Number:12345
+        </Typography>
+      </Card>
+    </div>
   );
 }
 
