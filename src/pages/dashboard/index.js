836ffcaf9b9168ffb46a8c95a411784fb87b7e45
hm-gowthami <gowthami.uppula@happiestminds.com>
2024-04-12 16:42:56 +0530

    chore: removed

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,5 +1,4 @@
 export * from "@/pages/dashboard/home";
-export * from "@/pages/dashboard/profile";
 export * from "@/pages/dashboard/uploadedFiles";
 export * from "@/pages/dashboard/student";
 export * from "@/pages/dashboard/teacher";
