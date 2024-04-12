eef9dc74beca6c02705848acd84bcc7b2b37a2b2
hm-gowthami <gowthami.uppula@happiestminds.com>
2024-04-12 16:31:58 +0530

    chore: deleted not using files

--- src/pages/dashboard/FeedBackComponent/imageComponent.jsx
+++
@@ -1,9 +0,0 @@
-const ImageComponent = () => {
-  return (
-    <div>
-      <img src={`${process.env.PUBLIC_URL}/img/exam-over.jpg`} alt="My Image" />
-    </div>
-  );
-};
-
-export default ImageComponent;
--- src/pages/dashboard/student.jsx
+++ src/pages/dashboard/student.jsx
@@ -5,19 +5,17 @@ import TableComponent from "./tableAns/tableAns";
 import DynamicForm from "./Assessment/dynamicInput";
 
 export function Student() {
-  let student=true
+  let student = true;
   return (
     <div className="mt-12 mb-8 flex flex-col gap-12">
       <Card>
-        <CardHeader variant="gradient"  className="mb-8 p-6">
+        <CardHeader variant="gradient" className="mb-8 p-6">
           <Typography variant="h4" color="Black">
             Uploaded Documents By Students
           </Typography>
         </CardHeader>
       </Card>
-      {/* <Check student={student}/> */}
-      <DynamicForm/>
-      {/* <TableComponent/> */}
+      <Check student={student} />
     </div>
   );
 }
