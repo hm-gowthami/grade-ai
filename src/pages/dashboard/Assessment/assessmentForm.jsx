607a149349a4e759f0a357b42b91dbadbfa82997
hm-gowthami <gowthami.uppula@happiestminds.com>
2024-04-19 16:04:33 +0530

    chore: dynamic inputs from frontend side only

--- src/pages/dashboard/Assessment/assessmentForm.jsx
+++ src/pages/dashboard/Assessment/assessmentForm.jsx
@@ -1,64 +1,117 @@
 import React, { useState } from "react";
 
-import { Input, Button, Textarea, Typography } from "@material-tailwind/react";
+import { input, Button, Textarea, Typography } from "@material-tailwind/react";
 import axios from "axios";
 import "./assessment.scss";
 import TableComponent from "../tableAns/tableAns";
 import Spinner from "./spinner";
+import { v4 as uuidv4 } from "uuid";
 
+function generateUniqueId() {
+  return uuidv4(); // Generates a UUID (e.g., 'f47ac10b-58cc-4372-a567-0e02b2c3d479')
+}
 const AssessmentFormPage = () => {
   const [formData, setFormData] = useState({
     question: "",
     total_marks: "",
     answer: "",
-    rubrics: "",
   });
   const [isLoading, setIsLoading] = useState(false);
   const [resultData, setResultData] = useState(null);
   const [error, setError] = useState("");
-
+  const [dynamicInputs, setDynamicInputs] = useState([
+    { id: generateUniqueId(), rubrics_text: "", rubric_mark: "" },
+  ]);
+  let rubricsCheck;
+  for (let i = 0; i < dynamicInputs.length; i++) {
+    rubricsCheck =
+      dynamicInputs[i].rubrics_text.trim() === "" ||
+      dynamicInputs[i].rubric_mark.trim() === "";
+  }
   const isFormIncomplete =
     formData.total_marks.trim() === "" ||
     formData.question.trim() === "" ||
-    formData.answer.trim() === "" ||
-    formData.rubrics.trim() === "";
-
+    formData.answer.trim() === "";
+  const totalCheck = rubricsCheck || isFormIncomplete;
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  // Function to handle input changes in dynamic fields
+  const handleDynamicInputChange = (index, event) => {
+    const newInputs = [...dynamicInputs];
+    newInputs[index][event.target.name] = event.target.value;
+    setDynamicInputs(newInputs);
+  };
+
+  // Function to add new input row to dynamic fields
+
+  const addDynamicInput = () => {
+    setDynamicInputs([
+      ...dynamicInputs,
+      { id: generateUniqueId(), rubrics_text: "", rubric_mark: "" },
+    ]);
+  };
+
+  const handleRemoveRow = (index) => {
+    let newRows = dynamicInputs.filter((item) => item.id !== index);
+    setDynamicInputs(newRows);
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (isFormIncomplete) {
+    if (totalCheck) {
+      // You could set an error message here if needed
       return;
     }
     setIsLoading(true);
-    try {
-      const response = await axios.post(
-        "http://13.234.113.33:5000/evaluate",
-        formData
-      );
-      if (response.status === 200) {
-        setResultData(response.data);
-      }
-    } catch (error) {
-      console.error("There was an error with the API call", error);
-    } finally {
-      setIsLoading(false);
+
+    const formPayload = structuredClone(formData);
+    let rubricsText = "";
+    for (let i = 0; i < dynamicInputs.length; i++) {
+      let a = dynamicInputs[i].rubrics_text;
+      let b = dynamicInputs[i].rubric_mark;
+      rubricsText = rubricsText + `${a}: ${b} marks; `;
     }
+    console.log("rubricsText", rubricsText);
+
+    let payload = {
+      question: formPayload.question,
+      total_marks: formPayload.total_marks,
+      answer: formPayload.answer,
+      rubrics: rubricsText && rubricsText,
+    };
+    console.log("payload", payload);
+    // try {
+    //   const response = await axios.post(
+    //     "http://3.109.182.96:5000/answerEvaluation",
+    //     payload
+    //   );
+    //   if (response.status === 200) {
+    //     console.log("res", response);
+    //     if (typeof response?.data === "object" && response !== null) {
+    //       setResultData(response?.data);
+    //     } else {
+    //       setError(response?.data);
+    //     }
+    //   }
+    // } catch (error) {
+    //   console.error("There was an error with the API call", error);
+    // } finally {
+    setIsLoading(false);
+    // }
   };
-  // style={{
-  //   border: '2px solid #ccc', // default border color
-  //   outline: 'none' // removes the default focus outline
-  // }}
 
   return (
     <div className="parent-div">
       <div>
         <form className="form-ass">
           <div className="text-center">
-            <Typography variant="h2" color="black" className="animated-text">
+            <Typography
+              variant="h2"
+              style={{ color: "#1b1b5e" }}
+              className="animated-text"
+            >
               {" "}
               Assessment
             </Typography>
@@ -66,15 +119,15 @@ const AssessmentFormPage = () => {
           <div className="div-input-parent">
             <div className="div-input-1">
               <Typography
-                variant="large"
-                color="black"
+                variant="h5"
+                style={{ color: "#1b1b5e" }}
                 className="mb-3 font-medium"
               >
                 Question
               </Typography>
-              <Input
+              <input
                 size="lg"
-                style={{ color: "black", fontSize: "18px" }}
+                style={{ color: "#1b1b5e", fontSize: "18px" }}
                 fullWidth
                 type="text"
                 name="question"
@@ -87,66 +140,140 @@ const AssessmentFormPage = () => {
             </div>{" "}
             <div className="div-input-2">
               <Typography
-                variant="large"
-                color="black"
+                variant="h5"
+                style={{ color: "#1b1b5e" }}
                 className="mb-3 font-medium"
               >
                 Marks
               </Typography>
-              <Input
+              <input
                 size="lg"
-                style={{ color: "black", fontSize: "18px" }}
+                min={0}
+                style={{ color: "#1b1b5e", fontSize: "18px" }}
                 fullWidth
-                type="text"
+                type="number"
                 name="total_marks"
                 value={formData.total_marks}
                 onChange={handleChange}
-                placeholder="Enter a text"
+                placeholder="0"
               />{" "}
             </div>
           </div>
           <div className="div-input">
             <Typography
-              variant="large"
-              color="black"
+              variant="h5"
+              style={{ color: "#1b1b5e" }}
               className="mb-3 font-medium"
             >
               Answer
             </Typography>
             <Textarea
-              style={{ color: "black", fontSize: "18px" }}
+              style={{ color: "#1b1b5e", fontSize: "18px" }}
               className="input-field"
               name="answer"
               value={formData.answer}
               onChange={handleChange}
-              color="lightBlue"
+              // color="lightBlue"
               size="regular"
               outline={true}
-              placeholder="Type your message here"
+              placeholder="Give your answer"
             />
           </div>
-
-          <div className="div-input-rubrics">
-            <Typography
-              variant="large"
-              color="black"
-              className="mb-3 font-medium"
+          <div
+            style={{
+              maxHeight: "300px",
+              overflowY: "auto",
+              overflowX: "hidden",
+              width: "100%",
+              // margin: "20px",
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                marginLeft: "10px",
+                marginBottom: "10px",
+                justifyContent: "space-between",
+              }}
             >
-              Rubrics
-            </Typography>
-            <Textarea
-              color="lightBlue"
-              name="rubrics"
-              value={formData.rubrics}
-              onChange={handleChange}
-              style={{ color: "black", fontSize: "18px" }}
-              size="regular"
-              outline={true}
-              placeholder="Type your message here"
-            />
+              <Typography
+                variant="h5"
+                style={{ color: "#1b1b5e", width: "80%" }}
+              >
+                Rubrics
+              </Typography>
+              <Typography
+                variant="h5"
+                style={{ color: "#1b1b5e", marginRight: "55px" }}
+              >
+                marks
+              </Typography>
+            </div>
+            {dynamicInputs.map((input, index) => (
+              <div
+                key={input.id}
+                style={{
+                  marginBottom: "10px",
+                  marginLeft: "10px",
+                  width: "100%",
+                  paddingRight: "20px",
+                }}
+                className="div-input-parent-rubric"
+              >
+                <div className="div-input-dyn-1">
+                  <input
+                    size="lg"
+                    style={{ color: "#1b1b5e", fontSize: "18px", width: "" }}
+                    fullWidth
+                    type="text"
+                    name="rubrics_text"
+                    value={input.rubrics_text}
+                    onChange={(e) => handleDynamicInputChange(index, e)}
+                    placeholder="Enter the rubrics"
+                  />{" "}
+                </div>{" "}
+                <div
+                  style={{
+                    width: "20%",
+                  }}
+                >
+                  <input
+                    size="lg"
+                    style={{
+                      color: "#1b1b5e",
+                      fontSize: "18px",
+                      width: "100%",
+                    }}
+                    type="text"
+                    name="rubric_mark"
+                    value={input.rubric_mark}
+                    onChange={(e) => handleDynamicInputChange(index, e)}
+                    placeholder="0"
+                  />{" "}
+                </div>
+                <div>
+                  <button
+                    onClick={addDynamicInput}
+                    className="add-btn"
+                    type="button"
+                  >
+                    <strong> + </strong>
+                  </button>
+                  {dynamicInputs.length > 1 && (
+                    <button
+                      onClick={() => handleRemoveRow(input.id)}
+                      className="remove-rubric-btn"
+                    >
+                      <strong> x </strong>
+                    </button>
+                  )}
+                </div>
+              </div>
+            ))}
           </div>
           <Button
-            disabled={isFormIncomplete}
+            disabled={totalCheck}
             className="button-ass"
             onClick={handleSubmit}
           >
@@ -154,21 +281,16 @@ const AssessmentFormPage = () => {
           </Button>
         </form>
       </div>
-      <div
-        style={{
-          marginTop: "30px",
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "center",
-        }}
-      >
-        {isLoading ? (
-          <Spinner />
-        ) : error ? (
-          <h2 style={{ color: "red" }}>{error}</h2>
-        ) : (
-          resultData && <TableComponent data={resultData} />
-        )}
+      <div style={{ marginTop: "30px", width: "100%" }}>
+        <div>
+          {isLoading ? (
+            <Spinner />
+          ) : error ? (
+            <h2 style={{ color: "red" }}>{error}</h2>
+          ) : (
+            resultData && <TableComponent data={resultData} />
+          )}
+        </div>
       </div>
     </div>
   );
